3eb5bc99e0790c180f7496a103c94684a89b6dd9
SaiNikhileshReddy <sainikhilaie@gmail.com>
2024-02-27 19:34:58 +0000

    Remove Notebooks & Stage PyPI changes

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import setuptools
+
+# The directory containing this file
+setuptools.setup(
+    name="fedem",
+    version="0.0.1",
+    description="A simple example package",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mlsquare/",
+    author="Seshu",
+    author_email="seshu@mlsquare.com",
+    license="MIT",
+    project_urls={
+        "Documentation": "mlsquare.com",
+        "Source": "https://github.com/mlsquare/seshu/",
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Large Language Models",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.10",
+    install_requires=["transformers", "torch"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
