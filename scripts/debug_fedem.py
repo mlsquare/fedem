c8aa1175a0ccdf1e6d4582027efc73374ac7a1a0
SaiNikhileshReddy <sainikhilaie@gmail.com>
2024-03-09 19:34:05 +0000

    Added Sphinx & Updated PyPI Workflow

--- scripts/debug_fedem.py
+++ scripts/debug_fedem.py
@@ -1,22 +1,22 @@
-# import sys
+import sys
 
-# PATH = "/workspaces/Experiments/fedem/"
+PATH = "/workspaces/Experiments/fedem/"
 
-# try:
-#     sys.path.index(PATH)
-# except ValueError:
-#     sys.path.append(PATH)
+try:
+    sys.path.index(PATH)
+except ValueError:
+    sys.path.append(PATH)
 
 
 from fedem.client import Seshu
 
 model = Seshu(
-    hf_model_path="mlsquare/pico_mamba",
+    hf_model_path="mlsquare/pico_seshu_test",
     hf_tokenizer_path="google/byt5-large",
-    target_modules=["out_proj"],
-    hf_adapter_path="mlsquare/exp-lora-ada-1",
-    hf_data_path="mlsquare/samantar1per_cent_merged_with_train_val",
-    hf_token="<hf_token>",
+    target_modules=["model.layers.3.dt_proj"],
+    hf_adapter_path="mlsquare/mamba_pico_small_dt_proj",
+    hf_data_path="mlsquare/CLIENT_samantar_mixed_train_val",
+    hf_token="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",
 )
 
 model.train_lora(debug=True)
